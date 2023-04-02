8ff4463b288927ba443bdd86fe98636470e4ba09
emblafant <embla98wallen@gmail.com>
2023-04-02 09:03:43 +0200

    -

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
 
 module.exports = {
   env: {
-    NEXT_PUBLIC_STRAPI_URL: "http://127.0.0.1:1337/api/",
+    NEXT_PUBLIC_STRAPI_URL: "http://127.0.0.1:1337/",
   },
   images: {
     remotePatterns: [
